96a852328bbe75163bd2e2d9a2aebbdd84176a19
TheOtherP <theotherp@gmx.de>
2016-01-07 21:26:34 +0100

    Travis

--- nzbhydra/tests/startTests.py
+++ nzbhydra/tests/startTests.py
@@ -2,7 +2,8 @@ from os.path import dirname, join, abspath
 from sys import path
 import pytest
 
-base_path = dirname(abspath(__file__))
-path.insert(0, join(base_path, '/../../nzbhydra'))
-path.insert(0, join(base_path, '/../../libs'))
+base_path = dirname(dirname(dirname(abspath(__file__))))
+path.insert(0, join(base_path, 'nzbhydra'))
+path.insert(0, join(base_path, 'libs'))
+print(path)
 pytest.main()
