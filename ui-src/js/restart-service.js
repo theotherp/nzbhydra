8d579a0d05a360f01ba4f520d12ff117cb8f4757
TheOtherP <theotherp@gmx.de>
2016-01-31 13:59:53 +0100

    Gulp

--- ui-src/js/restart-service.js
+++ ui-src/js/restart-service.js
@@ -8,36 +8,34 @@ function RestartService(blockUI, $timeout, $window, NzbHydraControlService) {
         restart: restart,
         countdownAndReload: countdownAndReload
     };
-    
-    function countdownAndReload(message) {
-            message = angular.isUndefined ? "" : " ";
 
-            blockUI.start(message + "Restarting. Will reload page in 5 seconds...");
+    function countdownAndReload(message) {
+        message = angular.isUndefined ? "" : " ";
+
+        blockUI.start(message + "Restarting. Will reload page in 5 seconds...");
+        $timeout(function () {
+            blockUI.start(message + "Restarting. Will reload page in 4 seconds...");
             $timeout(function () {
-                blockUI.start(message + "Restarting. Will reload page in 4 seconds...");
+                blockUI.start(message + "Restarting. Will reload page in 3 seconds...");
                 $timeout(function () {
-                    blockUI.start(message + "Restarting. Will reload page in 3 seconds...");
+                    blockUI.start(message + "Restarting. Will reload page in 2 seconds...");
                     $timeout(function () {
-                        blockUI.start(message + "Restarting. Will reload page in 2 seconds...");
+                        blockUI.start(message + "Restarting. Will reload page in 1 second...");
                         $timeout(function () {
-                            blockUI.start(message + "Restarting. Will reload page in 1 second...");
-                            $timeout(function () {
-                                blockUI.start("Reloading page...");
-                                $window.location.reload();
-                            }, 1000);
+                            blockUI.start("Reloading page...");
+                            $window.location.reload();
                         }, 1000);
                     }, 1000);
                 }, 1000);
             }, 1000);
-        
+        }, 1000);
     }
 
     function restart(message) {
         NzbHydraControlService.restart().then(countdownAndReload(message),
-        function() {
-            growl.info("Unable to send restart command.");
-        }
+            function () {
+                growl.info("Unable to send restart command.");
+            }
         )
-        
     }
 }
