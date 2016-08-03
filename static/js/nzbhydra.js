a5c60d5cde2b37c6bb459062b6060939d3d1b1fc
TheOtherP <theotherp@gmx.de>
2016-08-03 05:57:42 +0200

    Increase waiting time after restart, probably too high most times but whatever.

--- static/js/nzbhydra.js
+++ static/js/nzbhydra.js
@@ -2176,19 +2176,31 @@ function RestartService(blockUI, $timeout, $window, NzbHydraControlService) {
 
     function countdownAndReload(message) {
         message = angular.isUndefined ? "" : " ";
-
-        blockUI.start(message + "Restarting. Will reload page in 5 seconds...");
+        //THere has to be a better way for this
+        blockUI.start(message + "Restarting. Will reload page in 9 seconds...");
         $timeout(function () {
-            blockUI.start(message + "Restarting. Will reload page in 4 seconds...");
+            blockUI.start(message + "Restarting. Will reload page in 8 seconds...");
             $timeout(function () {
-                blockUI.start(message + "Restarting. Will reload page in 3 seconds...");
+                blockUI.start(message + "Restarting. Will reload page in 7 seconds...");
                 $timeout(function () {
-                    blockUI.start(message + "Restarting. Will reload page in 2 seconds...");
+                    blockUI.start(message + "Restarting. Will reload page in 6 seconds...");
                     $timeout(function () {
-                        blockUI.start(message + "Restarting. Will reload page in 1 second...");
+                        blockUI.start(message + "Restarting. Will reload page in 5 seconds...");
                         $timeout(function () {
-                            blockUI.start("Reloading page...");
-                            $window.location.reload();
+                            blockUI.start(message + "Restarting. Will reload page in 4 seconds...");
+                            $timeout(function () {
+                                blockUI.start(message + "Restarting. Will reload page in 3 seconds...");
+                                $timeout(function () {
+                                    blockUI.start(message + "Restarting. Will reload page in 2 seconds...");
+                                    $timeout(function () {
+                                        blockUI.start(message + "Restarting. Will reload page in 1 second...");
+                                        $timeout(function () {
+                                            blockUI.start("Reloading page...");
+                                            $window.location.reload();
+                                        }, 1000);
+                                    }, 1000);
+                                }, 1000);
+                            }, 1000);
                         }, 1000);
                     }, 1000);
                 }, 1000);
