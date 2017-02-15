8d6dac6e66d000effce3d90d93b7f7b01286388e
adam kaminski <thimslugga@users.noreply.github.com>
2017-02-15 10:30:22 -0500

    Update config.py
    This should generate a random API key at first run instead of using hardcoded API key.

--- nzbhydra/config.py
+++ nzbhydra/config.py
@@ -113,7 +113,7 @@ initialConfig = {
         }
     ],
     "main": {
-        "apikey": "ab00y7qye6u84lx4eqhwd0yh1wp423",
+        "apikey": os.urandom(12).encode('hex'),
         "branch": "master",
         "configVersion": 33,
         "dereferer": "http://www.dereferer.org/?$s",
